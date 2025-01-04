c0bf3cc658f444e3a7e1acd97f2551ea812365ee
duynguyeneiu <phamgiabao1112002@gmail.com>
2025-01-04 13:05:27 +0700

    4/1/2025

---
+++ LAB1_PhamGiaBao/LAB1_PhamGiaBao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LAB1_PhamGiaBao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
